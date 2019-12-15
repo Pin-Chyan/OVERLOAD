2e9c84a844766c2aa708bedd2f248ea694ba37c8
LiamKrieling <lmk310500@gmail.com>
2019-12-15 16:01:32 +0200

    imahe crud opps done

--- api/routes/home.route.js
+++ api/routes/home.route.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const mongoose = require('mongoose');
 
 router.route('/').get( (req, res) => {
-	res.json("<h>hello <h>");	
+	res.json('<h>hello<h>');
 });
 
 module.exports = router;
--- api/routes/image.routes.js
+++ api/routes/image.routes.js
@@ -1,15 +1,15 @@
 const router = require('express').Router();
 var fs = require('fs');
-let imgModels = require('../models/image.models.js');
+const imgModels = require('../models/image.models.js');
 
 function base64_encode(file) {
     var bitmap = fs.readFileSync(file);
     return bitmap.toString('base64');
 }
 
-router.route('/add').post( (req, res) => {
+router.route('/c').post( (req, res) => {
 	const username = req.body.username;
-	const img = 'data:image/' + req.body.imgType + ';base64, ' + base64_encode(req.body.path);
+	const img = 'this is the image bruh';//'data:image/' + req.body.imgType + ';base64, ' + base64_encode(req.body.path);
 	const imgType = req.body.imgType;
     const newimg = new imgModels({
         username,
@@ -21,7 +21,21 @@ router.route('/add').post( (req, res) => {
     .catch( err => res.status(400).json('Error: ' + err));
 });
 
-router.route('/remove').post( (req, res) => {
+router.route('/r').post( (req , res) => {
+    imgModels.find({"username" : req.body.username}).exec().then(docs => {
+        res.json({docs});
+    })
+})
+
+router.route('/u').post( (req, res) => {
+    imgModels.findOne({"username" : req.body.username}).exec().then(image => {
+        image.img = req.body.newimg;
+        image.save();
+        res.json('image updated');
+    });
+})
+
+router.route('/d').post( (req, res) => {
     if (req.body.id){
         imgModels.findOneAndDelete({ _id: req.body.id }, function (err) {
             if(err) res.json(err);
@@ -29,11 +43,13 @@ router.route('/remove').post( (req, res) => {
         });
     }
     else{
-        imgModels.findOneAndDelete({ username: req.body.username }, function (err) {
+        imgModels.findOneAndDelete({ "username" : req.body.username }, function (err) {
             if(err) res.json(err);
             res.json("image deleted using username");  
         });
     }
 })
+
+
 
 module.exports = router;
