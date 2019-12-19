06b6aaa6b9acea073201046516dcf6ccc8f32f0f
Liam Krieling <lkrielin@c3r6s9.wethinkcode.co.za>
2019-12-19 16:57:06 +0200

    home page construct

--- matcha-app/src/Carousel.js
+++ matcha-app/src/Carousel.js
@@ -1,32 +1,6 @@
 // import React from "react";
 // import { Carousel } from "react-responsive-carousel";
 
-<<<<<<< HEAD
-// export default () => (
-//   <Carousel autoPlay className="image img_carousel">
-    // <div>
-    //     <img alt="Asuna" className="m_image" src={require('./images/sen.jpg')} />
-    //     <p className="legend">Legend 1</p>
-    // </div>
-//     <div>
-//         <img alt="Asuna" className="m_image" src={require('./images/meave.jpg')} />
-//         <p className="legend">Legend 2</p>
-//     </div>
-//     <div>
-//         <img alt="Asuna" className="m_image" src={require('./images/sup.jpg')} />
-//         <p className="legend">Legend 3</p>
-//     </div>
-//     <div>
-//         <img alt="Asuna" className="m_image" src={require('./images/err.jpg')} />
-//         <p className="legend">Legend 4</p>
-//     </div>
-//     <div>
-//         <img alt="Asuna" className="m_image" src={require('./images/kawaii.jpg')} />
-//         <p className="legend">Legend 4</p>
-//     </div>
-//   </Carousel>
-// );
-=======
 export default () => (
   <Carousel autoPlay className="image img_carousel">
     <div>
@@ -51,4 +25,3 @@ export default () => (
     </div>
   </Carousel>
 );
->>>>>>> e8ea21584e2a71799bef7b34c81242b6318bce6d
