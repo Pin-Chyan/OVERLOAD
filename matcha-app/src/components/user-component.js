de05395d317fbb0e3d768959d5242359bdf7b83c
Liam Krieling <lkrielin@c3r6s9.wethinkcode.co.za>
2019-12-19 08:13:40 +0200

    git error

--- matcha-app/src/components/user-component.js
+++ matcha-app/src/components/user-component.js
@@ -21,7 +21,6 @@ export default class User extends Component {
 
     componentDidMount () {
         var name = "meave";
-        var ok;
         axios.post('http://localhost:5001/users/get', {"name":name}).then(res => {
             console.log(res.data[0]);
             this.setState({
