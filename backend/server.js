dd4a52e60eecfbbded9c630904ce52edeeaa28c5
marthenVde <marthen.vandereems@gmail.com>
2020-01-10 12:09:18 +0200

    merging

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const msg_schem = require('./models/chats.models');
 const client = require('socket.io').listen(4001).sockets;
 const exp = require('./socket/socket');
-const auth = require('./middleware/auth.js');
+const auth = require('./auth/auth.middleware');
 const authRoutes  = require('./routes/auth.routes.js');
 
 var connectedUsers = [];
@@ -33,11 +33,6 @@ const userRoutes = require('./routes/user.routes.js');
 app.use('/users', userRoutes);
 app.use('/auth', authRoutes);
 app.use('/users', userRoutes);
-app.use('/chats', chatRoutes);
-app.use('/img', imgRoutes);
-
-// =======
-// app.use('/users', auth.verifyToken , userRoutes);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
