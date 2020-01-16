e637a9e42440eddd5390b80a936892d32aa64511
LiamKrieling <lmk310500@gmail.com>
2020-01-16 07:58:42 +0200

    route page restructure

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -7,6 +7,10 @@ const nodemailer = require('nodemailer');
 const md5 = require('md5');
 require('dotenv').config();
 
+///////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//                  <<<< Email Routes >>>>
+//
 const mailData = { email: 'marvan.matcha.testservice@gmail.com', password: 'Noticeme'}
 
 const transporter = nodemailer.createTransport({
@@ -36,6 +40,53 @@ router.route('/sendResetLink').post((req, res) => {
     res.send('email sent');
 });
 
+router.route('/emailVerify/:vkey').post((req, res) => {
+    const { vkey } = req.params;
+
+    UserModels.find({ "vKey": vkey }).exec().then(docs => {
+        if (docs[0].verified === true) {
+            res.json({status: "already used"});
+        } else if (docs[0].verified === false) {
+            docs[0].verified = true;
+            docs[0].save();
+            res.json({ status: "activated"});
+        } else {
+            res.json({ status: "something went wrong"});
+        }
+    }).catch(err => {
+        res.json({ status: "not found"});
+    })
+});
+
+router.post('/getEmail', verifyToken, (req, res) => {
+    if (!req.token) {
+        res.sendStatus(403);
+    }
+    jwt.verify(req.token, process.env.SECRET, (err, decoded) => {
+        if (err) {
+            res.sendStatus(403);
+        } else {
+            UserModels.find({ "email": decoded.email}).exec().then(docs => {
+                if (!docs[0]) {
+                    res.sendStatus(404);
+                }
+                res.json({ email: docs[0].email });
+            })
+        }
+    })
+});
+
+router.route('/email').post( (req, res) => {
+    UserModels.find({ "email": req.body.email}).exec().then(docs => {
+        res.json({'present' : docs.length});
+    }).catch(err => {res.status(500).send(err)});
+})
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//                      <<<< User Routes >>>>
+//
 router.route('/add').post( (req, res) => {
     const name = req.body.name;
     const last = req.body.last;
@@ -81,136 +132,24 @@ router.route('/add').post( (req, res) => {
     }));
 });
 
-router.route('/emailVerify/:vkey').post((req, res) => {
-    const { vkey } = req.params;
-
-    UserModels.find({ "vKey": vkey }).exec().then(docs => {
-        if (docs[0].verified === true) {
-            res.json({status: "already used"});
-        } else if (docs[0].verified === false) {
-            docs[0].verified = true;
-            docs[0].save();
-            res.json({ status: "activated"});
-        } else {
-            res.json({ status: "something went wrong"});
-        }
-    }).catch(err => {
-        res.json({ status: "not found"});
-    })
-});
-
-router.route('/like').post( (req, res) => {
-    if (!req.body.token && !req.body.email && !req.body.target)
-        req.json("error");
-    UserModels.find({"email": req.body.target}, "_id").exec().then(docs => {
-            UserModels.findOne({"email": req.body.email}, "likes").exec().then(docs2 => {
-                if (!docs2.likes.includes(docs[0]._id)){
-                    var like = docs2.likes;
-                    like.push(docs[0]._id);
-                    docs2.likes = like;
-                    docs2.save().then(r => {res.json("liked")}).catch(err => {res.json(err)});
-                }
-                else
-                res.json("Already Liked!");
-            })
-    }).catch(err => {res.json(err)})
-})
-
-router.route('/Del_like').post( (req, res) => {
-    if (!req.body.token || !req.body.target || !req.body.email)
-        req.json("error");
-    UserModels.find({"email": req.body.target}, "_id").exec().then(docs => {
-            UserModels.findOne({"email": req.body.email}, "likes").exec().then(docs2 => {
-                if (docs2.likes.includes(docs[0]._id)){
-                    var index = docs2.likes.findIndex(function (ret){return ret === docs[0]._id});
-                    docs2.likes.splice(index,1);
-                    docs2.save().then(r => {res.json("Like removed")}).catch(err => {res.json(err)});
-                }
-                else
-                res.json("Not Liked");
-            })
-    }).catch(err => {res.json(err)})
-})
-
 router.route('/get_spec').post( (req, res) => {
-    // console.log(req.body);
     if (req.body.token)
         if (req.body.target != "")
             UserModels.find({ "email": req.body.email},req.body.target + " token").exec().then(docs => {
-                // console.log(docs[0]);
-                // console.log(docs[0].token);
-                // console.log(req.body.token);
                 if ((req.body.token == docs[0].token) || (req.body.token == "admin"))
                     res.json(docs);
                 else
-                    res.json("invalid token");
-            })
+                    res.status(403).send("invalid token");
+            }).catch(err => {res.status(500).send(err)});
         else
-            res.json("no target");
+            res.status(400).send("no target");
     else
-        res.json("token not present");
+        res.status(400).send("token not present");
 })
-
-router.route('/get_next').post( (req, res) => {
-    // console.log(req.body);
-    if (req.body.token)
-            UserModels.find({ "email": req.body.email},req.body.target + " token").exec().then(docs => {
-                if ((req.body.token == docs[0].token) || (req.body.token == "admin")){
-                    UserModels.find({},"img email name tag like last bio").exec().then(doc2 => {
-                        var data = {};
-                        data.max = doc2.length;
-                        var pos = req.body.position;
-                        if (doc2.find(function (res){return res.email == req.body.email;}))
-                            data.max--;
-                        if (doc2[pos].email == req.body.email){
-                            if (pos + 1 > data.max)
-                                res.json("end");
-                            else
-                                pos++;
-                        }
-                        data.ret = doc2[pos];
-                        // doc2[pos].max = data.max;
-                        res.json(data);
-                    }).catch(err => {console.log(err)})
-                }
-                else
-                    res.json("invalid token");
-            }).catch(err => {console.log(err)})
-        else
-            res.json("no target");
-})
-
-router.post('/getEmail', verifyToken, (req, res) => {
-    if (!req.token) {
-        res.sendStatus(403);
-    }
-    jwt.verify(req.token, process.env.SECRET, (err, decoded) => {
-        if (err) {
-            res.sendStatus(403);
-        } else {
-            UserModels.find({ "email": decoded.email}).exec().then(docs => {
-                if (!docs[0]) {
-                    res.sendStatus(404);
-                }
-                res.json({ email: docs[0].email });
-            })
-        }
-    })
-});
-
-function sleep(milliseconds) {
-    const date = Date.now();
-    let currentDate = null;
-    do {
-      currentDate = Date.now();
-    } while (currentDate - date < milliseconds);
-}
 
 router.route('/edit_spec').post( (req, res) => {
     if (req.body.token){
         UserModels.find({'email':req.body.email}).exec().then(doc => {
-            // console.log(req.body.token);
-            // console.log(doc[0].token);
             if ((req.body.token == doc[0].token || req.body.token == "admin") && (req.body.token != "")) {
                 UserModels.findOne({'email':req.body.email}).exec().then(doc => {
                     if (req.body.name)
@@ -245,41 +184,101 @@ router.route('/edit_spec').post( (req, res) => {
                         if (req.body.img.img5)
                             doc.img.img5 = req.body.img.img5;
                     }
-                    // sleep(2000);
-                    doc.save().then(r => {res.json("saved")}).catch(err => {res.json(err)});
+                    doc.save().then(r => {res.status(200).send("saved")}).catch(err => {res.status(500).send(err)});
                 })
             }
             else 
-                res.json("Invalid Token");
+                res.status(403).send("Invalid Token");
         })
     }
     else
-        res.json("no Token Present");
+        res.status(400).send("no Token Present");
 })
 
-router.route('/email').post( (req, res) => {
-    UserModels.find({ "email": req.body.email}).exec().then(docs => {
-        res.json({'present' : docs.length});
-    })
+router.route('/get_next').post( (req, res) => {
+    if (req.body.token)
+            UserModels.find({ "email": req.body.email},req.body.target + " token").exec().then(docs => {
+                if ((req.body.token == docs[0].token) || (req.body.token == "admin")){
+                    UserModels.find({},"img email name tag like last bio").exec().then(doc2 => {
+                        var data = {};
+                        data.max = doc2.length;
+                        var pos = req.body.position;
+                        if (doc2.find(function (res){return res.email == req.body.email;}))
+                            data.max--;
+                        if (doc2[pos].email == req.body.email){
+                            if (pos + 1 > data.max)
+                                res.status(204).send("end");
+                            else
+                                pos++;
+                        }
+                        data.ret = doc2[pos];
+                        res.json(data);
+                    }).catch(err => {console.log(err)})
+                }
+                else
+                    res.status(403).send("invalid token");
+            }).catch(err => {res.status(500).send(err)})
+        else
+            res.status(400).send("no target");
 })
 
+///////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//                      <<<< Like Routes >>>>
+//
+router.route('/like').post( (req, res) => {
+    if (!req.body.token && !req.body.email && !req.body.target)
+        req.json("error");
+    UserModels.find({"email": req.body.target}, "_id").exec().then(docs => {
+            UserModels.findOne({"email": req.body.email}, "likes").exec().then(docs2 => {
+                if (!docs2.likes.includes(docs[0]._id)){
+                    var like = docs2.likes;
+                    like.push(docs[0]._id);
+                    docs2.likes = like;
+                    docs2.save().then(r => {res.json("liked")}).catch(err => {res.json(err)});
+                }
+                else
+                res.json("Already Liked!");
+            })
+    }).catch(err => {res.json(err)})
+})
+
+router.route('/Del_like').post( (req, res) => {
+    if (!req.body.token || !req.body.target || !req.body.email)
+        req.json("error");
+    UserModels.find({"email": req.body.target}, "_id").exec().then(docs => {
+            UserModels.findOne({"email": req.body.email}, "likes").exec().then(docs2 => {
+                if (docs2.likes.includes(docs[0]._id)){
+                    var index = docs2.likes.findIndex(function (ret){return ret === docs[0]._id});
+                    docs2.likes.splice(index,1);
+                    docs2.save().then(r => {res.json("Like removed")}).catch(err => {res.json(err)});
+                }
+                else
+                res.json("Not Liked");
+            })
+    }).catch(err => {res.json(err)})
+})
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//                      <<<< Message Routes >>>>
+//
 router.route('/notify').post( (req, res) => {
     if (!req.body.token && req.body.target && req.body.msg && req.body.notify)
-        req.json("error");
+        req.status(400).send("error");
     UserModels.find({'email':req.body.email},"token").exec().then(doc => {
         if (req.body.token == "admin" || doc.token == req.body.token) {
             UserModels.findOne({'email':req.body.target}, "notify").exec().then(ret => {
-                console.log(ret);
                 var msg = ret.notify;
                 msg.push(req.body.notify);
                 ret.notify = msg;
-                console.log(msg);
-                ret.save().then(r => {res.json("saved")}).catch(err => {res.json(err)});
+                ret.save().then(r => {res.status(200).send("saved")}).catch(err => {res.status(500).send(err)});
             })
         }
         else
-            res.json("error");
-    }).catch(err => {res.json(err)})
+            res.status(403).send("error");
+    }).catch(err => {res.status(500).send(err)})
 })
 
 router.route('/msg').post( (req, res) => {
@@ -321,7 +320,5 @@ router.route('/msg_del').post( (req, res) => {
             res.json("error");
     }).catch(err => {res.json(err)})
 })
-
-
 
 module.exports = router;
