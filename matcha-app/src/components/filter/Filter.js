dd219b29466ae742ab26bbf707958a91e1aa7266
Pin-Chyan <tsaipexqiupc99@gmail.com>
2020-01-29 13:43:41 +0200

    filter changes

--- matcha-app/src/components/filter/Filter.js
+++ matcha-app/src/components/filter/Filter.js
@@ -24,6 +24,28 @@ export default class Filter extends React.Component {
                         Bisexual
                     </label>
                 </div>
+            <label className="label center_b search-t">Fame</label>
+                <div className="center_b">
+                    <label className="radio c-margin search-r">
+                        <input type="radio" name="fame" value="" onChange={this.on} />
+                        Most
+                    </label>
+                    <label className="radio c-margin search-r">
+                        <input type="radio" name="fame" value="" onChange={this.on} />
+                        Least
+                    </label>
+                </div>
+            <label className="label center_b search-t">Distance</label>
+                <div className="center_b">
+                    <label className="radio c-margin search-r">
+                        <input type="radio" name="distance" value="-1" onChange={this.on} />
+                        Closest
+                    </label>
+                    <label className="radio c-margin search-r">
+                        <input type="radio" name="distance" value="1" onChange={this.on} />
+                        Furthest
+                    </label>
+                </div>
             </div>
         )
     }
