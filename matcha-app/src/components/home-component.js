069a2be5f4268684fdbf5b1ba50b7440ccb957a9
LiamKrieling <lmk310500@gmail.com>
2020-01-30 17:37:39 +0200

    fix bi gender search error

--- matcha-app/src/components/home-component.js
+++ matcha-app/src/components/home-component.js
@@ -76,7 +76,7 @@ export default class Home extends Component {
             }
         }
         var req = {};
-        req.targ = [[0,100],-2,-2,this.state.user.sexual_pref,this.state.user.gender,-1,-1];
+        req.targ = [[0,100],-2,-2,this.state.user.sexual_pref === 0 ? -2 : this.state.user.sexual_pref,this.state.user.gender,-1,-1];
         req.in = '';
         get_matches(this.state.user.email,this.jwt,this.ip,req).then(res => {
             if (res !== 'no result'){
@@ -309,7 +309,7 @@ export default class Home extends Component {
                     </span>
                 </div>
                 <a className="navbar-item " style={{color:this.state.other_page}} id='/notification' onClick={this.redirecthandler}><Inbox redirectHandler={() => this.props.history.push('/notification')}/></a>
-                <a className="navbar-item " style={{color:this.state.other_page}}  id='/mychats' onClick={this.redirecthandler}><i class="fa fa-comments" id="/mychats"></i></a>
+                <a className="navbar-item " style={{color:this.state.other_page}}  id='/mychats' onClick={this.redirecthandler}><i className="fa fa-comments" id="/mychats"></i></a>
                 <a className="navbar-item " style={{color:this.state.other_page}} id='/' onClick={this.redirecthandler}>Home</a>
                 <a className="navbar-item " style={{color:this.state.curr_page}} id='/user' onClick={this.redirecthandler}>Profile</a>
                 <a className="navbar-item " style={{color:this.state.other_page}} id='/edit' onClick={this.redirecthandler}>Profile Editor</a>
