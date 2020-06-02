30c080227ea6c592f5fcbcc549cc3832d92a8cc6
liamkrieling <lmk310500@gmail.com>
2020-06-02 06:45:21 +0200

    configured senpai

--- sqlBack/initdb.js
+++ sqlBack/initdb.js
@@ -1,25 +1,34 @@
 var mysql = require('mysql');
-
-var con = mysql.createConnection({
-  host: "localhost",
-  port : 1111,
-  user: "senpai",
-  password: "noticeme",
-  database: "senpai"
-});
+var usr = {
+    host: "localhost",
+    port : 1111,
+    user: "senpai",
+    password: "noticeme",
+    database: "senpai"
+}
+var con = mysql.createConnection(usr);
+usr.password = 'notyourharem(this is not the actual password)';
 
 var tables = [
-    { table : "customers", schema : "(name VARCHAR(255), address VARCHAR(255))"},
+    { table : "testTable", schema : "(name VARCHAR(255), address VARCHAR(255))"},
 ];
 
+console.log("Contacting Senpai....");
 con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected....");
+  if (err) throw (["Cannot aquire senpai's attention", err.code, err.address + ':' + err.port]);
+  console.log("Aquired Senpai's attention");
+  console.log(" ");
+  console.log("Configuring Senpai's noticing schemas");
   Promise.all(drop_tables(con, tables)).then((res) => {
     console.log(res);
     Promise.all(build_tables(con, tables)).then((res2) => {
         console.log(res2);
-        console.log('done');
+        console.log(' ');
+        console.log('Senpai noticing has been configured');
+        console.log('notice me specifications :');
+        console.log(usr);
+        console.log(' ')
+        console.log('Senpai can now notice on port 1111');
         process.exit();
     })
   });
@@ -49,7 +58,6 @@ function req(senpai, query){
         })
     })
 }
-
 
 //likes (shane) ( user_id , liked , likes)
 //images (liam) ( user_id , img1, img2 ......)
