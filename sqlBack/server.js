026e16b281fb94387992ddadef85ce4020d39e6f
liamkrieling <lmk310500@gmail.com>
2020-06-06 19:03:05 +0200

    fixed body parser

--- sqlBack/server.js
+++ sqlBack/server.js
@@ -15,19 +15,14 @@ const usr = {
 const senpai = mysql.createConnection(usr)
 senpai.connect((err) => {
     if (err) console.log(["Cannot aquire senpai's attention", err.code, err.address + ':' + err.port]);
-    else host();
-})
-function host(){
-    app.use(cors());
-    app.use(bodyParser());
-    app.use(bodyParser.urlencoded({ extended: false }))
-    app.use(bodyParser.json())
-    app.use(express.json({limit: '50mb'}));
-    app.listen(port, () => {
+    else app.listen(port, () => {
         console.log(`Server is running on port: ${port}`);
     });
-}
+})
 // routes
+app.use(cors());
+app.use(bodyParser());
+app.use(express.json({limit: '50mb'}));
 app.get('/test', (req,res) => {
     request(senpai, "CREATE TABLE likes( `user_id` VARCHAR(255), `target` TEXT, `likedBy` TEXT)").then((res) => {
         console.log(res);
@@ -39,9 +34,7 @@ app.post('/upimg', (req,res) => {
     res.json("done");
 });
 
-function uploadimg(req_body){
-    return request(senpai, "INSERT INTO image (img1,img2,img3,img4,img5) VALUES (" + req_body.img1 + "," + req_body.img2 + "," + req_body.img3 + "," + req_body.img4 + "," + req_body.img5 + ")")
-}
+// functions
 function request(senpai, query){
     return new Promise((resolve) => {
         senpai.query(query, (err, res) => {
