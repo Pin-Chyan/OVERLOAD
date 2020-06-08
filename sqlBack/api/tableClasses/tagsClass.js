b0693ced8293bae8f5110e145e2063316c2347a8
Pin-Chyan <tsaipexqiupc99@gmail.com>
2020-06-08 18:36:54 +0200

    small fixture

--- sqlBack/api/tableClasses/tagsClass.js
+++ sqlBack/api/tableClasses/tagsClass.js
@@ -1,21 +1,20 @@
 class tags{
 
     constructor(senpai){
-        this.senpa
-        i = senpai;
+        this.senpai = senpai;
     }
 
-    insert_tag(senpai, userid, num, name){
-        var pos = "tag" + num;
-        console.log(pos);
-        var sql = "INSERT INTO tags (?) VALUES (?))"
-        console.log(userid)
-        console.log(name);
-        senpai.query(sql,[pos, name], function(err,result){
-            if (err) throw err;
-            console.log("result.insertID")
-        });
-    }
+    // insert_tag(senpai, userid, num, name){
+    //     var pos = "tag" + num;
+    //     console.log(pos);
+    //     var sql = "INSERT INTO tags (?) VALUES (?))"
+    //     console.log(userid)
+    //     console.log(name);
+    //     senpai.query(sql,[pos, name], function(err,result){
+    //         if (err) throw err;
+    //         console.log("result.insertID")
+    //     });
+    // }
 
     test(){
         return ('tags online');
