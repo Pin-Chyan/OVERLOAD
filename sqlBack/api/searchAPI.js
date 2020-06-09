bd4fd5b345a6cf7685d35cf93b725556ff29a07e
liamkrieling <lmk310500@gmail.com>
2020-06-09 18:25:29 +0200

    saearch api build 1

--- sqlBack/api/searchAPI.js
+++ sqlBack/api/searchAPI.js
@@ -9,11 +9,47 @@ class searchAPI{
     }
         
     query(req, res){
-        res.json(this.test());
+        var body = req.body;
+        if (body.controller == 'test'){
+            this.search(body).then((result) => {
+                res.json(result);
+            })
+        } else {
+            res.json('error unknown controller');
+        }
     }
 
     test(){
         return ('searchAPI online');
+    }
+
+    async search(requestBody){
+        var userData = await this.get_user(requestBody.user);
+        console.log(userData.data.user);
+    }
+    async get_user(user_id){
+        console.log(user_id);
+        var user = await this.request.read('users', { "user_id":user_id });
+        var location = await this.request.read('locations', { "user_id":user_id });
+        var likes = await this.request.read('likes', { "user_id":user_id });
+        if (user.status == 'Success' && location.status == 'Success' && likes.status == 'Success'){
+            return({
+                "status":"success",
+                "data":{
+                    "user":user.data[0],
+                    "location":location.data[0],
+                    "likes":likes.data[0]
+                }
+            })
+        } else {
+            return ({
+                "status":"error",
+                "data":"null"
+            });
+        }
+        // console.log(user);
+        // console.log(location);
+        // console.log(likes);
     }
 }
 
