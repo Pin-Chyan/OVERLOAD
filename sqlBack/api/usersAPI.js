e50742eb93263c9a64f636e12e03910fd348af0f
Shane Olivari (Group) <shane.olivari@global.ntt>
2020-06-15 14:04:57 +0200

    seriously why am i yellow?

--- sqlBack/api/usersAPI.js
+++ sqlBack/api/usersAPI.js
@@ -28,6 +28,12 @@ class usersAPI{
         } else if (body.controller == 'update_likes'){
             console.log(body.controller);
             this.update_likes(body).then((result) => { res.json(result); })
+        } else if (body.controller == 'remove_like'){
+            console.log(body.controller);
+            this.remove_like(body).then((result) => { res.json(result); })
+        } else if (body.controller == 'get_likes'){
+            console.log(body.controller);
+            this.get_likes(body).then((result) => { res.json(result); })
         } else {
             res.json('error unknown controller');
         }
@@ -148,18 +154,45 @@ class usersAPI{
             "user_id":requestBody.user
         });
     }
+    
+    async selected_tags(requestBody){
+        var i = 1;
+        var tags = {};
+        while (i < 6){
+            if (requestBody.args["tag" + i])
+            tags["tag" + i] = requestBody.args["tag" + i];
+            i++;
+        }
+        var res = await this.request.update('tags', tags,{
+            "user_id":requestBody.user
+        });
+    }
+    
+    async get_tags(requestBody){
+        return this.request.read('tags', {
+            "user_id":requestBody.user
+        });
+    }
+
+    //>>>> Like routes <<<<//
 
     async update_likes(requestBody){
+        //Setting variables
         var userdata = await this.request.read('likes',{
             "user_id":requestBody.user
         });
-        console.log(userdata);
+        var userdata2 = await this.request.read('likes',{
+            "user_id":requestBody.args.target
+        });
         var targets = userdata.data[0].target.split(',');
-        var likedby = userdata.data[0].likedBy.split(',');
-        if (targets.includes(requestBody.args.target) || likedby.includes(requestBody.user))
+        var likedby = userdata2.data[0].likedBy.split(',');
+        
+        //Error checking
+        if (targets.includes(requestBody.args.target))
             return ("Already liked");
         if (requestBody.user_id == requestBody.args.target)
             return ("You cannot like yourself");
+        
         //Adding users to tables
         if (targets[0] == '')
             targets[0] = requestBody.args.target;
@@ -169,9 +202,11 @@ class usersAPI{
             likedby[0] = requestBody.user;
         else
             likedby.push(requestBody.user);
+        
         //converting back to string
         var reqtargets = targets.toString();
         var reqlikedby = likedby.toString();
+
         //Actual request
         var res = await this.request.update('likes', {
             "target":reqtargets
@@ -186,24 +221,52 @@ class usersAPI{
         return ("likes updated");
     }
 
-    async selected_tags(requestBody){
-        var i = 1;
-        var tags = {};
-        while (i < 6){
-            if (requestBody.args["tag" + i])
-                tags["tag" + i] = requestBody.args["tag" + i];
-            i++;
-        }
-        var res = await this.request.update('tags', tags,{
-                "user_id":requestBody.user
+    async remove_like(requestBody){
+        var i = 0;
+        var userdata = await this.request.read('likes',{
+            "user_id":requestBody.user
         });
+        var userdata2 = await this.request.read('likes',{
+            "user_id":requestBody.args.target
+        });
+        var targets = userdata.data[0].target.split(',');
+        var likedby = userdata2.data[0].likedBy.split(',');
+        if (!targets.includes(requestBody.args.target) || !likedby.includes(requestBody.user))
+            return ("Not liked");
+        
+        //Removal
+        while(targets[i] != requestBody.args.target)
+            i++;
+        targets.splice(i, 1);
+        i = 0;
+        while (likedby[i] != requestBody.user)
+            i++;
+        likedby.splice(i, 1);
+
+        //converting back to string
+        var reqtargets = targets.toString();
+        var reqlikedby = likedby.toString();
+
+        //Actual request
+        var res = await this.request.update('likes', {
+            "target":reqtargets
+        }, {
+            "user_id":requestBody.user
+        });
+        var res2 = await this.request.update('likes', {
+            "likedby":reqlikedby
+        }, {
+            "user_id":requestBody.args.target
+        });
+        return ("like removed");
     }
 
-    async get_tags(requestBody){
-        return this.request.read('tags', {
+    async get_likes(requestBody){
+        return this.request.read('likes',{
             "user_id":requestBody.user
         });
     }
+
 }
 
 module.exports = { usersAPI };
