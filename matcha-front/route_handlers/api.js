5f90c57c67db62ba675a753b000dc1b1dd8e5162
cyko <lmk310500@gmail.com>
2020-12-09 08:21:11 +0200

    removing auth from api routes for  dev purposes

--- matcha-front/route_handlers/api.js
+++ matcha-front/route_handlers/api.js
@@ -1,14 +1,14 @@
 const router = require('express').Router();
-const {ensureAuthenticated} = require('./config/auth');
+const { ensureAuthenticated } = require('./config/auth');
 
 router.use('/auth', require('./routes/auth.js'));
-// router.use('/msg', require('./routes/msg.js'));
-// router.use('/usr', require('./routes/user.js'));
-// router.use('/search', require('./routes/search.js'));
-// router.use('/likes', require('./routes/likes.js'));
-router.use('/msg',  ensureAuthenticated, require('./routes/msg.js'));
-router.use('/usr',  ensureAuthenticated, require('./routes/user.js'));
-router.use('/search',   ensureAuthenticated, require('./routes/search.js'));
-router.use('/likes',    ensureAuthenticated, require('./routes/likes.js'));
+router.use('/msg', require('./routes/msg.js'));
+router.use('/usr', require('./routes/user.js'));
+router.use('/search', require('./routes/search.js'));
+router.use('/likes', require('./routes/likes.js'));
+// router.use('/msg',  ensureAuthenticated, require('./routes/msg.js'));
+// router.use('/usr',  ensureAuthenticated, require('./routes/user.js'));
+// router.use('/search',   ensureAuthenticated, require('./routes/search.js'));
+// router.use('/likes',    ensureAuthenticated, require('./routes/likes.js'));
 
 module.exports = router;
