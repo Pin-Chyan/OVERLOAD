aae48ddfde9dcf97d5a446eb0ca9c02c1c04fd3c
cyko <lmk310500@gmail.com>
2020-12-14 12:24:24 +0200

    added online routes

--- route_handlers/routes/user.js
+++ route_handlers/routes/user.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const db = require('../database/db');
+const bcrypt = require('bcrypt');
 
 // for db connection
 const connection = new db.dbConn();
@@ -21,10 +22,35 @@ router.route('/me').get( (req, res) => {
     });
 })
 
-router.route('/updateMe').get( (req, res) => {
-    if (!req.query.id){
+router.route('/updatedata').post( (req, res) => {
+    if (!req.body.id){
         return end(res,401,"an id was not specified");
     }
+
+    var promiseArr = [];
+    let keys = Object.keys(req.body);
+    keys.forEach(key => {
+        if (key != "id" && key != "password")
+            promiseArr.push(connection.update('users', key , req.body[key], req.body.id));
+    });
+    Promise.all(promiseArr).then((result) => {
+        console.log(result);
+        end(res, 200, "done");
+    })
+})
+
+router.route('/updatepass').post( (req, res) => {
+    if (!req.body.id){
+        return end(res,401,"an id was not specified");
+    }
+
+    if (!req.body.password){
+        return end(res,401,"new password was not specified");
+    }
+
+    hashPassword(req.body.password, req.body.id).then(() => {
+        end(res, 200, "done");
+    })
 })
 
 router.route('/img').post( (req,res) => {
@@ -56,9 +82,38 @@ router.route('/img').post( (req,res) => {
     })
 })
 
+router.route('/online').get((req, res) => {
+    if (!req.query.id){
+        return end(res,401,"an id was not specified");
+    }
+
+    let io = req.app.get('io');
+    let clients = req.app.get('clients');
+    const socketIdRec = clients[req.body.rec];
+    if (socketIdRec != undefined){
+        end(res, 200, "online");
+    } else {
+        end(res, 200, "offline");
+    }
+})
+
 function end(res, status, msg){
     res.status(status);
     res.json(msg);
+    return;
+}
+
+async function hashPassword (password , id) {
+    const saltRounds = 10;
+  
+    const hashedPassword = await new Promise((resolve, reject) => {
+      bcrypt.hash(password, saltRounds, function(err, hash) {
+        if (err) reject(err)
+        resolve(hash)
+      });
+    })
+
+    await connection.update('users', 'password', hashedPassword, id);
     return;
 }
 
