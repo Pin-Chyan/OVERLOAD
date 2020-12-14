c94fb4a7a7aad5cb9483f40899f113bd54b20161
Marthen <marthen.vandereems@gmail.com>
2020-12-14 16:34:38 +0200

    fixin stuff

--- scripts/home.js
+++ scripts/home.js
@@ -1,6 +1,6 @@
-// getData();
+getData();
 async function getData() {
-    const response = await fetch('api/usr/me?id=' + id);
+    const response = await fetch('api/usr/me?id=2');
     const data = await response.json();
     document.getElementById("name").innerHTML= data.name + ", " + data.age;
     document.getElementById("info").innerHTML= data.location;
