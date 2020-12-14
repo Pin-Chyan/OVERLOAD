65f6501d2096194898caef408ac75569eb8b7501
Marthen <marthen.vandereems@gmail.com>
2020-12-14 13:37:25 +0200

    moved fontend socket io stuff to notifications and getting notifications from db

--- scripts/notifications.js
+++ scripts/notifications.js
@@ -1,12 +1,33 @@
+let notifications = []
+
+var socket = io()
+socket.on('notification', function(data) {
+	notifications.push(data)
+	console.log(notifications)
+})
+
+
+getNotifications();
+async function getNotifications() {
+	// get user id
+	const response = await fetch('api/notifications/?id=3');
+	notifications = await response.json();
+
+	console.log(notifications)
+}
+
+function readNotifications() {
+}
+
+function updateNotifications(message) {
+	notifications.push(message);
+	// refresh element
+}
+
+function updateBtn() {
+}
+
 document.getElementById('toast-test').addEventListener('click', function () {
 	$(".toast").css({"top":"3.5rem","z-index": "101"});
 	$('.toast').toast('show');
 });
-
-getLogged();
-async function getLogged() {
-    const response = await fetch('api/usr/me?id=2');
-    const data = await response.json();
-    document.getElementById("loggedInUser").innerHTML= data.name;
-    console.log(data);
-}
