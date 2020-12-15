2429e81310c8618a61628b64e293d9f2ee0426eb
bilko94 <43352301+bilko94@users.noreply.github.com>
2020-12-15 17:54:09 +0200

    Cleaned likes.js

--- route_handlers/routes/likes.js
+++ route_handlers/routes/likes.js
@@ -4,15 +4,22 @@ const axios = require('axios');
 const apiUrl = 'http://localhost:' + process.env.WEBHOSTPORT + '/api';
 const db = require('../database/db');
 
-// for db connection
+///////// for db connection /////////
+/////////
+/////////
+
 const connection = new db.dbConn();
 
 // JSON body scheme: 
 // {
-//      "id":1,
-//      "email": "your email",
-//      "target":user id
+//      "id":1,                     - Required
+//      "target": targeted user id  - Optional (Req for functions where relevent)
 // }
+
+
+///////// LIKE ROUTES AND FUNCTIONS /////////
+/////////
+/////////
 
 router.route('/like').post( (req, res) => {
     liked_handler(req, res,"add");
@@ -37,6 +44,94 @@ async function liked_handler(req, res, check){
 
 }
 
+async function unlike(req, res) {
+    var query = "DELETE from likes WHERE (id= '"+ req.body.id +"' AND liked= '"+ req.body.target +"')";
+        
+    // fame, match and blocked handler
+    // fame_handle(req, "increase");
+    // match_handle(req,docs[0]._id, sender, reciever);
+    
+    // assigning like to db
+    var result = await connection.request(query);
+
+    // reading response
+    if (result.status == 'success'){
+        unmatch(req);
+        return end(res,200,"User Unliked!");
+    }
+    else 
+        return end(res,500,"error");
+}
+
+async function like(req, res) {
+    console.log("running like");
+    var query = "INSERT INTO likes (id, liked) VALUES('"+ req.body.id +"','"+ req.body.target +"')";
+    
+    // fame and blocked handler
+    // fame_handle(req, "increase");
+    // match_handle(req,docs[0]._id, sender, reciever);
+    
+    // assigning like to db
+    var result = await connection.request(query);
+
+    // reading response
+    if (result.status == 'success'){
+        checkMatch(req);
+        return end(res,200,"User liked");
+    }
+    else 
+        return end(res,500,"error");
+}
+
+
+///////// Block functions /////////
+/////////
+/////////
+
+router.route('/unblock').post( (req, res) => {
+
+    var query = "DELETE from blocked WHERE blocked='"+req.body.target+"' AND id='"+req.body.id+"'";
+    var request = connection.request(query);
+
+    request.then((result) => {
+        if (result.status == 'success')
+            return end2(res, "User unblocked");
+        else
+            return end2(res, "error");
+    });
+})
+
+router.route('/block').post( (req, res) => {
+
+    var query = "SELECT * from blocked WHERE blocked= '"+req.body.target+"'";
+    var request = connection.request(query);
+
+    request.then((result) => {
+        if (result.data.length == 0){
+            block(req);
+            return end2(res, "user blocked")
+        }
+        else 
+            return end2(res,"User already blocked");
+    });
+})
+
+async function block(req) {
+
+    var query = "INSERT INTO blocked (id, blocked) VALUES('"+req.body.id+"','"+req.body.target+"')";
+
+    var request = await connection.request(query)
+
+    if (request.status == 'success')
+        return;
+    else
+        return end(res, 500, "error");
+}
+
+///////// Notification handler /////////
+/////////
+/////////
+
 async function notification_handler(id) {
     connection.get("users", id).then((result) => {
         var msg = "You matched with "+result.data[0].name+"! send them a message.";
@@ -56,6 +151,26 @@ async function notification_handler(id) {
                 console.log("error")
         });
     })
+}
+
+///////// Match functions /////////
+/////////
+/////////
+
+async function checkMatch(req){
+
+    var query = "SELECT * from likes WHERE liked='"+req.body.id+"' AND id='"+req.body.target+"'";
+    var request = await connection.request(query);
+
+    if (request.data.length > 0){
+        console.log("match found");
+        addmatch(req.body.id, req.body.target);
+        notification_handler(req.body.id);
+        notification_handler(req.body.target);
+    } else{
+        console.log("no current match");
+        console.log(request.data);
+    }
 }
 
 async function addmatch(id1, id2){
@@ -89,49 +204,9 @@ async function unmatch(req){
     }
 }
 
-async function checkMatch(req){
-
-    var query = "SELECT * from likes WHERE liked='"+req.body.id+"' AND id='"+req.body.target+"'";
-    var request = await connection.request(query);
-
-    if (request.data.length > 0){
-        console.log("match found");
-        addmatch(req.body.id, req.body.target);
-        notification_handler(req.body.id);
-        notification_handler(req.body.target);
-    } else{
-        console.log("no current match");
-        console.log(request.data);
-    }
-}
-
-router.route('/unblock').post( (req, res) => {
-
-    var query = "DELETE from blocked WHERE blocked='"+req.body.target+"' AND id='"+req.body.id+"'";
-    var request = connection.request(query);
-
-    request.then((result) => {
-        if (result.status == 'success')
-            return end2(res, "User unblocked");
-        else
-            return end2(res, "error");
-    });
-})
-
-router.route('/block').post( (req, res) => {
-
-    var query = "SELECT * from blocked WHERE blocked= '"+req.body.target+"'";
-    var request = connection.request(query);
-
-    request.then((result) => {
-        if (result.data.length == 0){
-            block(req);
-            return end2(res, "user blocked")
-        }
-        else 
-            return end2(res,"User already blocked");
-    });
-})
+///////// Fame handler /////////
+/////////
+/////////
 
 router.route('/getfame').post( (req, res) => {
 
@@ -149,78 +224,9 @@ router.route('/getfame').post( (req, res) => {
     });
 })
 
-async function block(req) {
-
-    var query = "INSERT INTO blocked (id, blocked) VALUES('"+req.body.id+"','"+req.body.target+"')";
-
-    var request = await connection.request(query)
-
-    if (request.status == 'success')
-        return;
-    else
-        return end(res, 500, "error");
-}
-
-async function unlike(req, res) {
-    var query = "DELETE from likes WHERE (id= '"+ req.body.id +"' AND liked= '"+ req.body.target +"')";
-        
-    // fame, match and blocked handler
-    // fame_handle(req, "increase");
-    // match_handle(req,docs[0]._id, sender, reciever);
-    
-    // assigning like to db
-    var result = await connection.request(query);
-
-    // reading response
-    if (result.status == 'success'){
-        unmatch(req);
-        return end(res,200,"User Unliked!");
-    }
-    else 
-        return end(res,500,"error");
-}
-
-
-async function like(req, res) {
-    console.log("running like");
-    var query = "INSERT INTO likes (id, liked) VALUES('"+ req.body.id +"','"+ req.body.target +"')";
-    
-    // fame and blocked handler
-    // fame_handle(req, "increase");
-    // match_handle(req,docs[0]._id, sender, reciever);
-    
-    // assigning like to db
-    var result = await connection.request(query);
-
-    // reading response
-    if (result.status == 'success'){
-        checkMatch(req);
-        return end(res,200,"User liked");
-    }
-    else 
-        return end(res,500,"error");
-}
-
-
-
-// router.route('/viewed').post( (req, res) => {
-//     if (!req.body.token || !req.body.email || !req.body.target)
-//         res.json("empty fields");
-//     UserModels.find({"email": req.body.email}, "_id name last").exec().then(data => {
-//         UserModels.findOne({ "email": req.body.target}, "viewed").exec().then(docs => {
-//             if (docs.viewed.includes(data[0]._id))
-//                 res.json("already viewed!");
-//             else {
-//                 sender = data[0].name+data[0].last;
-//                 notification_handle(req, "viewed", sender)
-//                 var array = docs.viewed;
-//                 array.push(data[0]._id);
-//                 docs.viewed = array;
-//                 docs.save().then(() => {res.json("viewed")})
-//             }
-//         })
-//     }).catch(err => {res.json(err)});
-// })
+///////// Return functions /////////
+/////////
+/////////
 
 function end(res, status, msg){
     res.status(status);
