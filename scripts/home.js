712b687cfe303d405b6487cd6ca7fc3fc4137471
Pin-Chyan <tsaipexqiupc99@gmail.com>
2020-12-17 14:56:51 +0200

    fame, btns added content (home page)

--- scripts/home.js
+++ scripts/home.js
@@ -1,7 +1,24 @@
 getData();
 async function getData() {
+    // id of relative user found for match
     const response = await fetch('api/usr/me?id=' + id);
     const data = await response.json();
+
+    // get fame via post
+	const fresponse = await fetch('api/likes/getfame', {
+        method : 'POST',
+        mode : 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body : JSON.stringify({
+            // id of the userfound in homepage
+			'id': id,
+        })
+    })
+    const fdata = await fresponse.json();
+
+
     document.getElementById("name").innerHTML= data.name + ", " + data.age;
     document.getElementById("info").innerHTML= data.location;
 
@@ -25,18 +42,51 @@ async function getData() {
 		// document.body.append(span);
     }
     
-    document.getElementById("bio").innerHTML= data.bio;
+    document.getElementById("bio").innerHTML = data.bio;
+    document.getElementById("fame").innerHTML = "Fame: " + fdata;
     console.log(data);
+    console.log(fdata);
 }
 
 function like() {
-
-}
-
-function dislike() {
-
+	const response = fetch('api/likes/like', {
+        method : 'POST',
+        mode : 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body : JSON.stringify({
+            // id of the userfound in homepage
+			'id': id,
+        })
+    })
 }
 
 function block() {
-    
+	const response = fetch('api/likes/block', {
+        method : 'POST',
+        mode : 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body : JSON.stringify({
+            // id of current user
+            'id': id,
+            //  target if of block user
+            'target': id_2,
+        })
+    })
+}
+
+function next() {
+    const response = fetch('api/', {
+        method : 'POST',
+        mode : 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body : JSON.stringify({
+
+        })
+    })
 }
