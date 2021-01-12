fe4151591c970b7a6ffc25995ca30f2695a1606e
Pin-Chyan <tsaipexqiupc99@gmail.com>
2021-01-12 12:27:58 +0200

    search page lists users defualtly

--- scripts/search.js
+++ scripts/search.js
@@ -1,8 +1,9 @@
+var userArray;
+
 async function applyFilters(){
     var minPop = document.getElementById('minPop').value;
     var ageRange = document.getElementById('ageDiff').value;
     var distance = document.getElementById('DistanceDiff').value;
-    var tags = document.getElementById('interests').value;
 
     var query = "/api/search?id=" + id;
 
@@ -12,20 +13,28 @@ async function applyFilters(){
     if (distance){
         query += "&distance=" + distance;
     }
+    if (minPop){
+        query += "&minFame=" + minPop;
+    }
+    
 
     const response = await fetch(query);
     console.log(query);
     const data = await response.json();
-    console.log(data);
-}
+    // console.log(data);
+    userArray = data;
+    console.log(userArray);
 
-function replace() {
+    // sorting frontend work with bottom part
+
     var inject = document.getElementById("matchesList");
     // clean default
     inject.innerHTML = "";
 
-    i = 0;
-    while (i++ < 6) {
+    var arrayLength = userArray.length;
+    for (var i = 0; i < arrayLength; i++) {
+        console.log(userArray[i]);
+        //Do something
         var col = document.createElement("div");
         col.className = "col-lg-4";
         
@@ -35,22 +44,57 @@ function replace() {
         var img = document.createElement("img");
         img.className = "shadow-lg img-300";
         img.alt = "profile_img";
-        img.src = "https://i.pinimg.com/564x/7c/32/31/7c32313848b48fb9a564cfa312999c84.jpg";
+        img.src = userArray[i].img1;
         anchor.append(img);
         col.append(anchor);
         
         var p = document.createElement("p");
         p.className = "caveat rem-profile";
         // username
-        p.appendChild(document.createTextNode("Aniva"))
+        p.appendChild(document.createTextNode(userArray[i].name));
         p.appendChild(document.createElement("br"));
         // distance
-        p.appendChild(document.createTextNode("10 Milers"))
+        p.appendChild(document.createTextNode(userArray[i].distance));
         p.appendChild(document.createElement("br"));
         // age
-        p.appendChild(document.createTextNode("21"))
+        p.appendChild(document.createTextNode(userArray[i].age));
 
         col.append(p);
         inject.append(col);
     }
+
 }
+
+// function replace() {
+    
+
+//     // i = 0;
+//     // while (i++ < 6) {
+//     //     var col = document.createElement("div");
+//     //     col.className = "col-lg-4";
+        
+//     //     var anchor = document.createElement("a");
+//     //     anchor.setAttribute("href","/home");
+
+//     //     var img = document.createElement("img");
+//     //     img.className = "shadow-lg img-300";
+//     //     img.alt = "profile_img";
+//     //     img.src = "https://i.pinimg.com/564x/7c/32/31/7c32313848b48fb9a564cfa312999c84.jpg";
+//     //     anchor.append(img);
+//     //     col.append(anchor);
+        
+//     //     var p = document.createElement("p");
+//     //     p.className = "caveat rem-profile";
+//     //     // username
+//     //     p.appendChild(document.createTextNode("Aniva"))
+//     //     p.appendChild(document.createElement("br"));
+//     //     // distance
+//     //     p.appendChild(document.createTextNode("10 Milers"))
+//     //     p.appendChild(document.createElement("br"));
+//     //     // age
+//     //     p.appendChild(document.createTextNode("21"))
+
+//     //     col.append(p);
+//     //     inject.append(col);
+//     // }
+// }
